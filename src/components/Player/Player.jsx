63ad2f83e31988cc39e0aed9875392e67e1f50ab
Hamidreza Safarpour <hamidreza.safarpour01@gmail.com>
2024-11-15 17:10:59 +0330

    add-player-state
    just add state for change span into input

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditing() {
+    setIsEditing(!isEditing);
+  }
+
   return (
     <li>
-      <span className="palyer">
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" required></input>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      {!isEditing ? (
+        <button onClick={handleEditing}>Edit</button>
+      ) : (
+        <button onClick={handleEditing}>Save</button>
+      )}
     </li>
   );
 }
