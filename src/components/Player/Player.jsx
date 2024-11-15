515a0cc3d9168afe7281b51db08448f30b66864b
Hamidreza Safarpour <hamidreza.safarpour01@gmail.com>
2024-11-15 18:22:26 +0330

    add-edit-player
    add edit feature in player name

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
+  function hangleChange(event) {
+    setPlayerName(event.target.value);
+  }
   function handleEditing() {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   }
 
   return (
     <li>
       <span className="player">
         {!isEditing ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" required></input>
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={hangleChange}
+          ></input>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
