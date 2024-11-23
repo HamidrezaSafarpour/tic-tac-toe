13c044f124f8bbc6c7486b496833daf1f1ad59dc
Hamidreza Safarpour <hamidreza.safarpour01@gmail.com>
2024-11-23 12:41:39 +0330

    change-game-over-ui
    change symbol winner to player winner

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   function hangleChange(event) {
@@ -8,6 +13,9 @@ export default function Player({ initialName, symbol, isActive }) {
   }
   function handleEditing() {
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   return (
